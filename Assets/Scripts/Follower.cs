ec5f7c61f0d870e8948b7cdf3be666301d8668a8
TyLanger <ty.langer16@gmail.com>
2020-12-20 00:11:53 -0600

    Proof of Concept
    Player records their movements.
    
    Follower plays those movements back with a time offset.
    
    Very hacked together at this point.

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+
+    public PlayerController followerTarget; // need to refactor to make more generic
+
+    public float timeOffset = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Time.time > timeOffset)
+        {
+            transform.position = followerTarget.history[0].position;
+            transform.rotation = followerTarget.history[0].rotation;
+            followerTarget.history.RemoveAt(0);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed = 10;
+    Vector2 input = Vector2.zero;
+
+    public List<PointInTime> history;
+    float historyLength = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        history = new List<PointInTime>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + new Vector3(input.x, 0, input.y), moveSpeed * Time.fixedDeltaTime);
+        Record();
+    }
+
+    void Record()
+    {
+        //PointInTime current = new PointInTime(transform.position, transform.rotation);
+
+        if(history.Count > historyLength / Time.fixedDeltaTime)
+        {
+            history.RemoveAt(history.Count - 1);
+        }
+
+        history.Add(new PointInTime(transform.position, transform.rotation));
+    }
+
+}
